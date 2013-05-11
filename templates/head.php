bee9821a8c076e76743d53c32333d8691f91869c
Rafael Cordones <rafael@cordones.me>
2013-05-11 16:25:38 +0000

    Added 'Life' a Conversation' title and 'Open Sans' Google Fonts config

--- templates/head.php
+++ templates/head.php
@@ -5,11 +5,15 @@
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title><?php wp_title('|', true, 'right'); ?></title>
+  <title><?php wp_title('|', true, 'right'); ?> | Life's a Conversation</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" type="image/x-icon" href="/wp-content/themes/roots/assets/img/rafael-cordones.ico">
 
   <?php wp_head(); ?>
 
-  <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' 
+type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Inconsolata:400,700' rel='stylesheet' type='text/css'>
+  <link rel="alternate" type="application/rss+xml" title="<?php echo 
+get_bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 </head>
